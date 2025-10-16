950d64f2aef6b3a14e8509a29e59bbc6017ec7ce
matias lazarte <matiasnicolaslazarte@gmail.com>
2025-10-16 07:57:05 -0300

    feat: temina componente TodoItem

--- Primer-Proyecto-React/src/ComponentesLista/TodoItem.jsx
+++ Primer-Proyecto-React/src/ComponentesLista/TodoItem.jsx
@@ -5,5 +5,22 @@ import { useState } from "react";
 const TodoList = () => {
     // Estado principal que maneja el array de tareas
 
-    const
-}
+    const TodoItem = ({ todo, onDelete,OnToggle }) => {
+        <div className={`todo-item ${todo.completed ? 'completed' : ''}`}>
+            <div className="todo-content">
+                <input type="checkbox" checked={todo.completed} onChange={() => onToggleComplete(todo.id)} className="checkbox" />
+
+                <div className="todo-info">
+                    <span className="todo-text">{todo.text}</span>
+                    <span className="todo-date">📅 {todo.date}</span>
+                </div>
+            </div>
+
+            <button onClick={() => onDelete(todo.id)} className="dalete-button" aria-label="Eliminar Tarea">
+             🗑️ Eliminar
+            </button>
+        </div>
+    };
+};
+
+export default TodoList;
