b714e3240c07b60d46288b49e1318025c2a07797
matias lazarte <matiasnicolaslazarte@gmail.com>
2025-10-19 06:51:01 -0300

    crea funcion flecha que calcula los resultados en el componente padre y agrego el llamado a ResultDisplay

--- Primer-Proyecto-React/src/ComponentesCal/TipCalculator.jsx
+++ Primer-Proyecto-React/src/ComponentesCal/TipCalculator.jsx
@@ -9,16 +9,27 @@ const [peopleCounter, setPeopleCounter]= useState(0);
 
 
  const result = () => {
-   const tipAmount = billAmount * (tipPercentaje/100);
+   const tipAmount = bill * (tipPercentaje/100);
+   const totalWithTip = bill + tipAmount 
    const TipPerPerson = tipAmount / peopleCounter;
- }
+   const totalPerPerson = totalWithTip / peopleCounter;
+
+
+   return{
+    tipAmount,
+    totalWithTip,
+    TipPerPerson,
+    totalPerPerson
+   };
+ };
 export const TipCalculator = () => {
   return (
     <div>
         <BillInput value={bill} onChange={setBill} />
-        <TipPercentaje value={tipPercentaje} onChange={setTipPercentaje} />
+        <TipPercentage value={tipPercentaje} onChange={setTipPercentaje} />
         <PeopleCounter value={peopleCounter} onChange={setPeopleCounter}/>
-        
+
+        <ResultDisplay/>    
     </div>
   )
 }
