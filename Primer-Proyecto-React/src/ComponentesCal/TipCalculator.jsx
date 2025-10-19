af6622c5cd00018907bdf9d339daea2ea6484ec4
matias lazarte <matiasnicolaslazarte@gmail.com>
2025-10-19 06:57:18 -0300

    Agrego un if por si es 0

--- Primer-Proyecto-React/src/ComponentesCal/TipCalculator.jsx
+++ Primer-Proyecto-React/src/ComponentesCal/TipCalculator.jsx
@@ -9,6 +9,16 @@ export const TipCalculator = () => {
   const [peopleCounter, setPeopleCounter] = useState(0);
 
   const calculateResults = () => {
+    
+    if(bill <=0 || peopleCounter <=0){
+      return{
+        tipAmount: 0,
+        totalWithTip: 0,
+        tipPerPerson: 0,
+        totalPerPerson: 0
+      };
+    };
+    
     const tipAmount = bill * (tipPercentaje / 100);
     const totalWithTip = bill + tipAmount;
     const TipPerPerson = tipAmount / peopleCounter;
