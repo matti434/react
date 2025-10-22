5c9b0bfc0ba344980208295764c50e0295569dfc
MatiasLazarte <matiasnicolaslazarte@gmail.com>
2025-10-22 12:48:30 -0300

    feat: fix input errors and add new Tipo input
    - Apply Bootstrap and grid layout to inputs
    - Update parent component to include 'tipo' in task
    - Create ManejoInput.css file

--- Primer-Proyecto-React/src/ComponentesList/EstadoPrincipal.jsx
+++ Primer-Proyecto-React/src/ComponentesList/EstadoPrincipal.jsx
@@ -1,32 +1,45 @@
-import React, { useState } from 'react'
-import ManejoInput from './ManejoInput.jsx'
-import RecibeToDo from './RecibeToDo.jsx'
+import React, { useState } from "react";
+import ManejoInput from "./ManejoInput.jsx";
 
 const EstadoPrincipal = () => {
-    
-    const [tarea,setTarea] = useState([
-      titulo,
-      descripcion,
-      fecha
-    ]);
+  const [tarea, setTarea] = useState({
+    titulo: "",
+    tipo: "",
+    descripcion: "",
+    fecha: "",
+  });
 
-    const [listaTareas,setListaTarea] = useState([]);
+  const [listaTareas, setListaTarea] = useState([]);
 
-    const manejarAgregarTarea = (nuevaTarea) =>{
-
-      if(nuevaTarea.trim() === ""){
-        
+  const manejarAgregarTarea = (nuevaTarea) => {
+    if (nuevaTarea.titulo.trim() !== "") {
+      if (nuevaTarea.fecha) {
+        const fecha = new Date(nuevaTarea.fecha);
+        if (isNaN(fecha.getTime())) {
+          alert("Fecha debe ser valida");
+          return;
+        }
       }
-       setListaTarea([...listaTareas,nuevaTarea]);
-       setTarea("");
+      setListaTarea([...listaTareas, nuevaTarea]);
+      setTarea({ titulo: "", tipo: "", descripcion: "", fecha: "" });
+    } else {
+      alert("El contenido de la tarea no puede estar vacio");
+      return;
     }
+  };
 
-
-    return (
-    <div>
-        <ManejoInput value= {tarea} onChange={setTarea} onAgregarTarea= {manejarAgregarTarea} />
-    </div>
-  )
-}
+  return (
+    <>
+      <h1 className="text-center m-4">Lista de Tareas</h1>
+      <div>
+        <ManejoInput
+          value={tarea}
+          onChange={setTarea}
+          onAgregarTarea={manejarAgregarTarea}
+        />
+      </div>
+    </>
+  );
+};
 
 export default EstadoPrincipal;
--- Primer-Proyecto-React/src/ComponentesList/ManejoInput.jsx
+++ Primer-Proyecto-React/src/ComponentesList/ManejoInput.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import "./ManejoInput.css";
 
 const ManejoInput = ({ value, onChange, onAgregarTarea }) => {
   const manejarCambios = (campo, valor) => {
@@ -10,30 +11,38 @@ const ManejoInput = ({ value, onChange, onAgregarTarea }) => {
 
   const funcionSubmit = (e) => {
     e.preventDefault();
-  };
-  const manejoButton = () => {
     onAgregarTarea(value);
   };
 
   return (
     <div className="text-center m-5">
-      <form action="" onSubmit={funcionSubmit}>
+      <form action="" onSubmit={funcionSubmit} className="row contenedor-form g-3">
         <input
+          className="col-6 p-2"
           placeholder="Agrega un titulo"
           type="text"
-          value={value.nombre}
+          value={value.titulo}
           onChange={(e) => manejarCambios("titulo", e.target.value)}
         />
         <input
-          placeholder="Descripcion"
+          className="col-6 p-2"
+          placeholder="Tipo de Tarea EJ:deporte"
           type="text"
+          value={value.tipo}
+          onChange={(e) => manejarCambios("tipo", e.target.value)}
+        />
+        <input
+          className="col-12 py-5"
+          placeholder="Descripcion"
+          type="textarea"
           value={value.descripcion}
           onChange={(e) => manejarCambios("descripcion", e.target.value)}
         />
         <input
+          className="col-6 p-2"
           placeholder="Agrega una fecha"
           type="date"
-          value={value.date}
+          value={value.fecha}
           onChange={(e) => manejarCambios("fecha", e.target.value)}
         />
         <button type="submit">Agregar Tarea</button>
