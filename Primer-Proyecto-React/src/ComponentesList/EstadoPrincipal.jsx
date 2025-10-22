65a02ffae5f2bce25632d4ccd01645fe263e3fd0
MatiasLazarte <matiasnicolaslazarte@gmail.com>
2025-10-22 11:22:33 -0300

    feat: update EstadoPrincipal and ManejoInput components
    - EstadoPrincipal: change value to an array of objects (nombre, descripcion, fecha)
    - ManejoInput: add form with onSubmit function, new inputs, and integrated button

--- Primer-Proyecto-React/src/ComponentesList/EstadoPrincipal.jsx
+++ Primer-Proyecto-React/src/ComponentesList/EstadoPrincipal.jsx
@@ -1,16 +1,27 @@
 import React, { useState } from 'react'
 import ManejoInput from './ManejoInput.jsx'
+import RecibeToDo from './RecibeToDo.jsx'
 
 const EstadoPrincipal = () => {
     
-    const [tarea,setTarea] = useState("");
+    const [tarea,setTarea] = useState([
+      titulo,
+      descripcion,
+      fecha
+    ]);
+
     const [listaTareas,setListaTarea] = useState([]);
 
     const manejarAgregarTarea = (nuevaTarea) =>{
 
+      if(nuevaTarea.trim() === ""){
+        
+      }
        setListaTarea([...listaTareas,nuevaTarea]);
        setTarea("");
     }
+
+
     return (
     <div>
         <ManejoInput value= {tarea} onChange={setTarea} onAgregarTarea= {manejarAgregarTarea} />
--- Primer-Proyecto-React/src/ComponentesList/ManejoInput.jsx
+++ Primer-Proyecto-React/src/ComponentesList/ManejoInput.jsx
@@ -1,24 +1,45 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 
-const ManejoInput = ({value,onChange,onAgregarTarea}) => {
-   
+const ManejoInput = ({ value, onChange, onAgregarTarea }) => {
+  const manejarCambios = (campo, valor) => {
+    onChange({
+      ...value,
+      [campo]: valor,
+    });
+  };
+
+  const funcionSubmit = (e) => {
+    e.preventDefault();
+  };
   const manejoButton = () => {
     onAgregarTarea(value);
-  }
-  
+  };
+
   return (
-    <div className='text-center m-5'>
-        <input 
-        placeholder='Agrega nueva tarea'
-        type="text"
-        value={value}
-        onChange={(e) => onChange(e.target.value)}
-         />
-        <button onClick={manejoButton}>
-          Agregar Tarea
-        </button>
+    <div className="text-center m-5">
+      <form action="" onSubmit={funcionSubmit}>
+        <input
+          placeholder="Agrega un titulo"
+          type="text"
+          value={value.nombre}
+          onChange={(e) => manejarCambios("titulo", e.target.value)}
+        />
+        <input
+          placeholder="Descripcion"
+          type="text"
+          value={value.descripcion}
+          onChange={(e) => manejarCambios("descripcion", e.target.value)}
+        />
+        <input
+          placeholder="Agrega una fecha"
+          type="date"
+          value={value.date}
+          onChange={(e) => manejarCambios("fecha", e.target.value)}
+        />
+        <button type="submit">Agregar Tarea</button>
+      </form>
     </div>
-  )
-}
+  );
+};
 
 export default ManejoInput;
