648f8a11c7fb50f35ede35d1efe0efb25bf049d5
matias lazarte <matiasnicolaslazarte@gmail.com>
2025-10-28 12:36:55 -0300

    feat: add Home

---
+++ Primer-Proyecto-React/src/Challenges/Challenge20/views/Home.jsx
@@ -0,0 +1,16 @@
+import { Container } from "react-bootstrap";
+
+
+const Home = () => {
+  return (
+    <Container className="mt-4">
+    <div className="text-center mb-5">
+       <h1>Bienvenido a MercadoLibre</h1>
+       <p className="lead">Encuentra los mejores productos al mejor precio</p>
+       
+    </div>
+    </Container>
+  )
+}
+
+export default Home;
