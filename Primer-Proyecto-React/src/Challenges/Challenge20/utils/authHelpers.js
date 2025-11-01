eb8c4ca4e5c2bff4b176f545a7666f98220bea22
matias lazarte <matiasnicolaslazarte@gmail.com>
2025-10-31 21:51:57 -0300

    chore: add keys

--- Primer-Proyecto-React/src/Challenges/Challenge20/utils/authHelpers.js
+++ Primer-Proyecto-React/src/Challenges/Challenge20/utils/authHelpers.js
@@ -1,24 +1,36 @@
+const CLAVE_USUARIOS = "usuarios";
+const CLAVE_BACKUP = "backupUsuarios";
+const CLAVE_USUARIO_ACTUAL = "usuarioActual";
+
+export const inicializarSistemaBackpu = () => {
+  if(!localStorage.getItem(CLAVE_BACKUP)){
+    localStorage.setItem(CLAVE_BACKUP,JSON.stringify([]));
+  }
+}
 
 
- export const guardarUsuario = (usuario,contraseña,email="")=>{
-    const usuarios = JSON.parse(localStorage.getItem('usuario')) || [];
 
-    if(usuarios.find(u => u.usuario === usuario)){
-        throw new Error("El usuario ya existe");
-    }
 
-    const nuevoUsuario= {usuario,contraseña,email};
-    usuario.push(nuevoUsuario);
-    localStorage.setItem('usuario',JSON.stringify(usuarios))
-    return true;
- }
+
+export const guardarUsuario = (usuario, contraseña, email = "") => {
+  const usuarios = JSON.parse(localStorage.getItem("usuario")) || [];
+
+  if (usuarios.find((u) => u.usuario === usuario)) {
+    throw new Error("El usuario ya existe");
+  }
+
+  const nuevoUsuario = { usuario, contraseña, email };
+  usuario.push(nuevoUsuario);
+  localStorage.setItem("usuario", JSON.stringify(usuarios));
+  return true;
+};
 
 export const validarLogin = (usuario, contraseña) => {
   // Primero verificar el estado de los datos
   verificarYOfrecerRestauracion();
-  
+
   const usuarios = JSON.parse(localStorage.getItem(CLAVE_USUARIOS)) || [];
-  const usuarioEncontrado = usuarios.find(u => 
-    u.usuario === usuario && u.contraseña === contraseña
+  const usuarioEncontrado = usuarios.find(
+    (u) => u.usuario === usuario && u.contraseña === contraseña
   );
-}
+};
