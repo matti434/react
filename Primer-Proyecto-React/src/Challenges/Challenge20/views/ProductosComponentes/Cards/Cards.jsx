6a2b8a0f3589d33844a616f6385b944e6d078531
matias lazarte <matiasnicolaslazarte@gmail.com>
2025-11-04 21:45:54 -0300

    feat: add initial structure

--- Primer-Proyecto-React/src/Challenges/Challenge20/views/ProductosComponentes/Cards/Cards.jsx
+++ Primer-Proyecto-React/src/Challenges/Challenge20/views/ProductosComponentes/Cards/Cards.jsx
@@ -1,59 +1,11 @@
-import { Container,Row,Col,Card } from "react-bootstrap";
-import { Link } from "react-router";
+import React from 'react'
 
-const Cards = () => {
+export const Cards = () => {
   return (
-     <Container fluid className="mt-5 mb-5 d-flex justify-content-center" >
-        <Row className="g-5">
-          <Col xs={12} md={6} lg={4}>
-            <Card as={Link} to="/producto/1" style={{ width: "18rem" }}>
-              <Card.Body>
-                <Card.Title className="text-center">#</Card.Title>
-                <Card.Text>
-                  <img
-                    src=""
-                    alt="cosadeMosquitos"
-                    style={{ height: "200px", objectFit: "cover" }}
-                  />
-                </Card.Text>
-                  <p className="text-center">#</p> 
-              </Card.Body>
-            </Card>
-          </Col>
-          <Col xs={12} md={6} lg={4}>
-            <Card style={{ width: "18rem" }}>
-              <Card.Body>
-                <Card.Title className="text-center">#</Card.Title>
-                <Card.Text>
-                  <img
-                    src=""
-                    alt="usb"
-                    style={{ height: "200px", objectFit: "cover" }}
-                  />
-                </Card.Text>
-                  <p className="text-center">#</p>
-              </Card.Body>
-            </Card>
-          </Col>
-          <Col xs={12} md={6} lg={4}>
-            <Card style={{ width: "18rem" }}>
-              <Card.Body>
-                <Card.Title className="text-center">#</Card.Title>
-                <Card.Text>
-                  <img
-                    src=""
-                    alt="stanley"
-                    style={{ height: "200px", objectFit: "cover" }}
-                  />
-                </Card.Text>
-                  <p className="text-center">#</p>
-                 
-              </Card.Body>
-            </Card>
-          </Col>
-        </Row>
-      </Container>
+    <div className='container'>
+        <div className='row'>
+          {productos.map()}
+        </div>
+    </div>
   )
 }
-
-export default Cards;
---
+++ Primer-Proyecto-React/src/Challenges/Challenge20/views/ProductosComponentes/Cards/ProductoCard.jsx
@@ -0,0 +1,8 @@
+
+
+const ProductoCard = ({ producto }) => {
+  return (
+
+
+  );
+};
